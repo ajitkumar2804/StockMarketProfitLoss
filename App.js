94ce68c6b3b06bfa7aae1e6667033e5feb91ca0b
ajitkumar2804 <ajitjagtap67@gmail.com>
2021-10-01 23:48:14 +0530

    Done with Logic And  Basic CSS

--- App.js
+++ App.js
@@ -1,30 +1,34 @@
 var initialPrice = document.querySelector("#enter-price");
-
 var quantity = document.querySelector("#quantity");
-
 var currentPrice = document.querySelector("#current");
-
 const btnCheck = document.querySelector("#check");
-
 const message = document.querySelector("#finalMessage");
 
-btnCheck.addEventListener("click",stockcalHandler);
+btnCheck.addEventListener("click", stockcalHandler);
 
-function stockcalHandler(){
-    var ip =Number(initialPrice.value);
-    var qty = Number(quantity.value);
-    var cp = Number(currentPrice.value);
+function stockcalHandler() {
+    var ip = initialPrice.value * quantity.value ;
+    var cp = currentPrice.value * quantity.value;
 
-    calProfitAndLoss(ip,qty,cp);
+
+    calProfitAndLoss(ip, cp);
 }
-function calProfitAndLoss(initial,quantity,current){
-    if(initial>current){
-        var loss = (initial-current)* quantity;
-        var lossPercentage = (loss/initial)*100;
+function calProfitAndLoss(initial, current) {
+    if (initial > current) {
+        var loss = (initial - current);
+        var lossPercentage = ((loss / initial) * 100).toFixed(2);
 
-    setMessage('The loss is ${loss} and the percentage is ${lossPercentage}%');
+        setMessage(`The loss is ${loss} and the Percentage is ${lossPercentage}%`);
+
+    } else if (current > initial) {
+        var profit = (current - initial);
+        var profitPercentage = ((profit / initial) * 100).toFixed(2);
+
+        setMessage(`The profit is ${profit} and the percentage is ${profitPercentage}%`);
+    } else {
+        setMessage('NO LOSS NO PROFIT')
     }
 }
-function setMessage(msg){
+function setMessage(msg) {
     message.innerHTML = msg;
 }
